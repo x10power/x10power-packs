0cc3e3b52726cc7cb9d2e012bf226affc5a09141
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-02-19 23:55:54 -0800

    Update CI

--- resources/ci/common/cleanup.py
+++ resources/ci/common/cleanup.py
@@ -1,15 +1,25 @@
+'''
+Cleanup workspace
+'''
 import json
 import os
 
 from shutil import rmtree
 
 CI_SETTINGS = {}
-with(open(os.path.join("resources","app","meta","manifests","ci.json"))) as ci_settings_file:
-  CI_SETTINGS = json.load(ci_settings_file)
+with open(
+    os.path.join(
+        "resources",
+        "app",
+        "meta",
+        "manifests",
+        "ci.json"
+    ), "r", encoding="utf-8") as ci_settings_file:
+    CI_SETTINGS = json.load(ci_settings_file)
 
 toNuke = []
 for nuke in CI_SETTINGS["common"]["cleanup"]["nuke"]:
-  toNuke.append(os.path.join(".",nuke))
+    toNuke.append(os.path.join(".", nuke))
 
 for nuke in toNuke:
-  rmtree(nuke)
+    rmtree(nuke)
